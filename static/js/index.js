6e7081145a7c967cf793a10550a0a0762bcf80bf
Tyler Cooper <tyler.cooper@tcnbroadcasting.com>
2015-10-06 12:05:54 -0600

    Updated server, added static files

---
+++ static/js/index.js
@@ -0,0 +1,66 @@
+($(document).ready(function() {
+    //var app = angular.module("AgentLogin", []);
+
+    var ws;
+    var usernameInput = $("#usernameInput");
+    var passwordInput = $("#passwordInput");
+    var loginButton = $("#loginButton");
+    var logoutButton = $("#logoutButton");
+
+    var urlIn = location.origin + "/api/v1/bb/web/login";
+    var urlOut = location.origin + "/api/v1/bb/web/logout";
+
+    function toast (msg, duration) {
+        Materialize.toast(msg, duration);
+    };
+
+    var token;
+
+    var ws = io(location.origin);
+
+    ws.on("loginReply", function (msg) {
+        console.log(msg);
+        token = msg.token;
+
+        if (msg.status == 200) {
+            toast("Logged in!", 5000);
+        }
+        else {
+            toast("Login failed!", 5000);
+        }
+    });
+
+    ws.on("loginReplyError", function (err) {
+        toast("Login failed!", 5000);
+        console.log(err);
+    });
+
+    ws.on("logoutReply", function(msg) {
+        toast("Logged out!", 5000);
+        console.log(msg);
+    });
+
+    loginButton.click(function() {
+        var username = usernameInput.val();
+        var password = passwordInput.val();
+
+        var msg = {
+            username: username,
+            password: password,
+            originationIp: "",
+            token: token
+        };
+
+        toast("Logging in. . . ", 5000);
+        ws.emit("login", msg);
+    });
+
+    logoutButton.click(function() {
+        var msg = {
+            token: token
+        };
+
+        toast("Logging out. . .", 5000);
+        ws.emit("logout", msg);
+    });
+}));
